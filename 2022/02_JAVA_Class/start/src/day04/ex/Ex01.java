b34a0018a2fcb36871382d4ab56093f6aadd462c
SEVIL-K <sevil0812@naver.com>
2022-02-28 11:23:26 +0900

    2022-02-28_11-23-25

--- 2022/02_JAVA_Class/start/src/day04/ex/Ex01.java
+++ 2022/02_JAVA_Class/start/src/day04/ex/Ex01.java
@@ -1,6 +1,4 @@
 package day04.ex;
-
-
 /*
  * 	문제 1 ]
  * 		1 ~ 25 숫자 3개를 랜덤하게 발생시켜서 그 수들 중에 가장 큰 수만 출력하는 프로그램을 작성하세요.
@@ -15,8 +13,30 @@ public class Ex01 {
 		int no2 = (int) (Math.random() * (25 - 1 + 1) + 1);
 		int no3 = (int) (Math.random() * (25 - 1 + 1) + 1);
 		
-		int biggest = (no1 > no2) && (no1 > no3) ? no1 : (no2 > no3? no2 : no3);
+		// 보너스 -100 ~ 100 사이의 정수를 랜덤하게 발생
+		// int bno = (int) (Math.random() * (100 - (-100) + 1) + (-100));
 		
+		
+		// 비교하기
+		int biggest = (no1 > no2) && (no1 > no3) ? no1 : (no2 > no3 ? no2 : no3);
+		int middle = no1 > no2 ? (no1 > no3 ? (no2 > no3 ? no2 : no3) : no1) : no2 > no3 ? (no1 > no3 ? no1 : no3) : no2;
+		int smallest = (no2 > no1) && (no3 > no1) ? no1 : (no2 > no3 ? no2 : no3);
+		
+		// 출력
 		System.out.println("랜덤한 숫자 " + no1 + ", " + no2 + ", " + no3 + " 중에서 가장 큰 숫자는 " + biggest + " 입니다.");
+		System.out.println("랜덤한 숫자 3개의 내림차순은 " + biggest + ", " + middle + ", " + smallest + "입니다.");
+		
+		/* 
+		 * 참고 ]
+		 * 		문자열을 표현할 때 문자열 안에 " 기호를 출력하는 방법
+		 * 			==> \"		이때 사용한 \ 를 이스케이프 문자라고 부른다.
+		 * 			
+		 * 			많이 사용하는 문자들
+		 * 			\"	문자열 내 " 표현
+		 * 			\n	줄바꿈
+		 * 			\r	캐리지 리턴 어쨌든 줄바꿈
+		 * 			\t	tab 표현
+		 * 			\b	backspace
+		 */
 	}
 }
--- 2022/02_JAVA_Class/start/src/day04/ex/Ex05.java
+++ 2022/02_JAVA_Class/start/src/day04/ex/Ex05.java
@@ -2,6 +2,8 @@ package day04.ex;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 /*
  	문제 5 ]
 		3자리 숫자(100 ~ 999)를 입력받아서
@@ -15,30 +17,51 @@ import java.util.Scanner;
 public class Ex05 {
 	public static void main(String[] args) {
 		
-		// 숫자 3개 입력받기
-
-		System.out.print("첫번째 숫자 : ");
-		Scanner sc1 = new Scanner(System.in);
-		System.lineSeparator();
 		
-		System.out.print("두번째 숫자 : ");
-		Scanner sc2 = new Scanner(System.in);
-		System.lineSeparator();
-		
-		System.out.print("세번째 숫자 : ");
-		Scanner sc3 = new Scanner(System.in);
-		System.lineSeparator();
-		
-		// 입력받은 문자열 저장
-		int no1 = sc1.nextInt();
-		int no2 = sc2.nextInt();
-		int no3 = sc3.nextInt();
-		sc1.close();
-		sc2.close();
-		sc3.close();
-		
+		// 숫자 입력받기
+		String sno = JOptionPane.showInputDialog("세 자리 정수 입력 : ");
+		// 입력받은 데이터는 문자열이므로 정수형태로 바꿔준다.
+		int cno = Integer.parseInt(sno);
+	
 		// 계산
-		int result = no1 ;
+		int cno_cal = (cno % 100) >= 50 ? 100 - (cno % 100) : -(cno % 100);
+		int conv_cal = cno_cal >= 0 ? cno_cal : -(cno_cal);
+		String cno_or1 = cno_cal >= 0 ? "더해야 한다" : "빼야 한다" ;
 		
+		// 출력
+		System.out.println(cno + " 은 " + (cno + cno_cal) + "에 가까우므로 " + conv_cal + "만큼 " + cno_or1);
+				
+		
+		/*
+		 * // 숫자 3개 입력받기
+		 * 
+		 * System.out.println("첫번째 숫자 : "); Scanner sc1 = new Scanner(System.in); int
+		 * no1 = sc1.nextInt();
+		 * 
+		 * System.out.println("두번째 숫자 : "); Scanner sc2 = new Scanner(System.in); int
+		 * no2 = sc2.nextInt();
+		 * 
+		 * System.out.println("세번째 숫자 : "); Scanner sc3 = new Scanner(System.in); int
+		 * no3 = sc3.nextInt();
+		 * 
+		 * 
+		 * sc1.close(); sc2.close(); sc3.close();
+		 * 
+		 * // 계산 int cal1 = (no1 % 100) >= 50 ? 100 - (no1 % 100) : -(no1 % 100); int
+		 * cal1_1 = cal1 >= 0 ? cal1 : -(cal1); String or1 = cal1 >= 0 ? "더해야 한다" :
+		 * "빼야 한다" ;
+		 * 
+		 * int cal2 = (no2 % 100) >= 50 ? 100 - (no2 % 100) : -(no2 % 100); int cal2_1 =
+		 * cal2 >= 0 ? cal2 : -(cal2); String or2 = cal2 >= 0 ? "더해야 한다" : "빼야 한다" ;
+		 * 
+		 * int cal3 = (no3 % 100) >= 50 ? 100 - (no3 % 100) : -(no3 % 100); int cal3_1 =
+		 * cal3 >= 0 ? cal3 : -(cal3); String or3 = cal3 >= 0 ? "더해야 한다" : "빼야 한다" ;
+		 * 
+		 * 
+		 * // 출력 System.out.println(no1 + " 은 " + (no1 + cal1) + "에 가까우므로 " + cal1_1 +
+		 * "만큼 " + or1 ); System.out.println(no2 + " 은 " + (no2 + cal2) + "에 가까우므로 " +
+		 * cal2_1 + "만큼 " + or2 ); System.out.println(no3 + " 은 " + (no3 + cal3) +
+		 * "에 가까우므로 " + cal3_1 + "만큼 " + or3 );
+		 */		
 	}
 }
