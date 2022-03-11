d7aa5e940f9bec91077392c791ebf081586f3872
SEVIL-K <sevil0812@naver.com>
2022-03-11 09:16:36 +0900

    2022-03-11_09-16-35

--- 2022/02_JAVA_Class/start/src/day10/pink/Black.java
+++ 2022/02_JAVA_Class/start/src/day10/pink/Black.java
@@ -1,14 +1,16 @@
-package day10;
+package day10.pink;
 
-/*
-	Black Pink 멤버의 정보를 기억할 배열을 만들고
-	데이터를 입력하고
-	완성이되면 내용을 출력하세요.
-	단, 각 멤버의 정보는 Friend 클래스에 기억시키도록 한다.
+/**
+ * 블랙핑크의 멤버를 모두 배열로 기억하고 출력
+ * @author	전은석
+ * @since	2022/03/09
+ * @version v.1.0
+ * @see		Member.Member() 생성자 참고
+ * 			Black.getMemb() 배열 반환해주는 메소드 참고
+ *
  */
-public class Test03 {
-	
-	int no ;
+
+public class Black {
 	String[] names;
 	String[] tels;
 	String[] email;
@@ -17,30 +19,21 @@ public class Test03 {
 	char gen ;
 	String addrs ;
 	
-	public Test03() {
-		// 멤버변수들 초기화
+	
+	public Black() {
 		setData();
 		
-		// 필요한 데이터들이 준비된 상태
+		Member[] memb = getMemb();
 		
-		// 각 멤버를 배열 하나로 관리
-		Friend[] black = getBlack();
-		
-		// 내용 출력
-		for(Friend f : black) {
-			System.out.println(f);
-			// 이렇게 하면 Friend 클래스에 재정의한 toString() 함수가 자동 호출된다.
-		}
-		
-		System.out.println();
-		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
-		
-		// 다시 출력
-		toPrint(black);
+		toPrint(memb);
 	}
 	
+	public static void main(String[] args) {
+		new Black();
+	}
+
 	// Friend 배열 내용 출력해주는 함수
-	public void toPrint(Friend[] b) {
+	public void toPrint(Member[] b) {
 		for(int i = 0 ; i < b.length ; i++ ) {
 			// 이때 배열의 각방에 기억된 객체들의 멤버변수는 접근이 불가능하므로
 			// 접근할 수 있는 함수를 호출해서 처리해야 한다.
@@ -64,30 +57,17 @@ public class Test03 {
 	}
 	
 	// 준비된 데이터를 Friend[] 타입의 데이터로 만들어서 반환해주는 함수
-	public Friend[] getBlack() {
+	public Member[] getMemb() {
 		// 이 함수는 Friend[] 배열을 만들고
-		Friend[] anne = new Friend[4]; // 아직 각 방에 Friend 타입의 객체는 들어있지 않고 기억할 공간만 확보한 상태
+		Member[] anne = new Member[4]; // 아직 각 방에 Friend 타입의 객체는 들어있지 않고 기억할 공간만 확보한 상태
 		
 		// 각 방에 Friend 객체들을 채워넣는다.
 		for(int i = 0 ; i < 4 ; i++ ) {
 			// 4명분 데이터가 입력되어야 하므로
 			// Friend 객체를 4개를 만들어서 각 방에 집어넣는다.
-			anne[i] = new Friend();
+			// 이때 필요한 정보도 입력해서 셋팅이 되도록 한다.
+			anne[i] = new Member(names[i], addrs, tels[i], email[i], birthday[i], naee[i], gen);
 			
-		}
-		// 위의 반복문이 종료가 되면 각 방에 있는 객체의 변수에 데이터를 입력할 수 있는 상태가 된다.
-		
-		// 데이터를 채워서
-		for(int i = 0 ; i < anne.length ; i++ ) {
-			Friend f = anne[i];
-			
-			String name = names[i];
-			f.setName(name);
-			f.setAddr(addrs);
-			f.setTel(tels[i]);
-			f.setMail(email[i]);
-			f.setBirth(birthday[i]);
-			f.setGen(gen);
 		}
 		
 		// 채원채의 배열을 반환해준다.
@@ -110,8 +90,4 @@ public class Test03 {
 		addrs = "ygM";
 	}
 	
-	public static void main(String[] args) {
-		new Test03();
-	}
-
 }
