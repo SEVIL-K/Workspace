41e76442907bb42e536a8a836a58add154f35cb8
SEVIL-K <sevil0812@naver.com>
2022-04-06 18:24:34 +0900

    2022-04-06_18-24-33

---
+++ 2022/02_JAVA_Class/db/soruce/day09/test02.sql
@@ -0,0 +1,24 @@
+-- test02 계정으로 접속해서 작업
+
+CREATE OR REPLACE VIEW shono
+AS
+SELECT no
+FROM test01;
+
+GRANT CREATE VIEW TO test01;
+GRANT SELECT ANY TABLE TO test01; -- 이건안됨. CREATE VIEW에 관련된 관리자권한만 얻어서
+
+REVOKE CREATE VIEW FROM test01;
+
+
+SELECT *
+FROM SCOTT.emp
+WHERE deptno = 10;
+
+
+SELECT *
+FROM jennie.member
+;
+
+-- test01에게 권한을 줄수 있는 권한까지 포함해서 scott.emp 를 조회하는 권한을 주세요.
+GRANT SELECT ON scott.emp TO test01 WITH GRANT OPTION;
