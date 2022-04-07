756d9c96ccd82bdd8a97d4ae812541dc7752b213
SEVIL-K <sevil0812@naver.com>
2022-04-07 12:19:11 +0900

    2022-04-07_12-19-10

--- 2022/02_JAVA_Class/strat_ITJ/src/examtest.java
+++ 2022/02_JAVA_Class/strat_ITJ/src/examtest.java
@@ -3,22 +3,18 @@
     
 */
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Set;
 
+
 public class examtest {
     public examtest() {
-        HashMap map = new HashMap();
 
-        map.put("이름", "홍길동");
-        map.put("나이", "24");
-        map.put("성별", "남자");
+    }
 
-        Set keys = map.keySet();
-        for(Object key : keys) {
-            Object value = map.get(key);
-            System.out.println(key + " - " + value);
-        }
+
+    public static void main(String[] args) {
+        new examtest();
     }
 }
-
