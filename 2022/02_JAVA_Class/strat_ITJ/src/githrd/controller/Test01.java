550670331ecfaefab77ac10788ec2e9ff007ea15
SEVIL-K <sevil0812@naver.com>
2022-04-27 12:20:31 +0900

    2022-04-27_12-20-31

---
+++ 2022/02_JAVA_Class/strat_ITJ/src/githrd/controller/Test01.java
@@ -0,0 +1,29 @@
+package githrd.controller;
+/*
+    Created by HG on 2022-04-27
+    
+*/
+
+public class Test01 {
+    public Test01() {
+    }
+
+
+    public void arrProc() {
+        String[] city = {"서울", "부산", "청주", "대구"};
+
+        for (String dosi : city) {
+            System.out.println(dosi);
+        }
+    }
+
+
+    // 출력 함수
+    public void printStar() {
+        /*
+            풀이 ]
+                안쪽 문자를 출력하는 횟수
+
+         */
+    }
+}
