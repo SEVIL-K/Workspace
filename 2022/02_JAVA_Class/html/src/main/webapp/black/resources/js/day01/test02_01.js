c862e68d309e726e7da8a3839921785c441715d2
SEVIL-K <sevil0812@naver.com>
2022-04-29 09:13:47 +0900

    2022-04-29_09-13-45

--- 2022/02_JAVA_Class/html/src/main/webapp/black/resources/js/day01/test02_01.js
+++ 2022/02_JAVA_Class/html/src/main/webapp/black/resources/js/day01/test02_01.js
@@ -1,3 +1,3 @@
-document.write('test02_01.js')
-document.write('<div style="color: red; font-size:20pt">첫번째 자바스크립트 파일</div>')
-document.write('<script src="/black/resources/js/day01/test02_02.js"></script>')
+document.write('test02_01.js 파일');
+document.write('<div style="color: blue; font-size: 20pt;">첫번째 파일에 추가되는 외부자바스크립트 파일</div>');
+document.write('<script type="text/javascript" src="/black/resources/js/day01/test02_02.js"></script>');
--- 2022/02_JAVA_Class/html/src/main/webapp/black/resources/js/day01/test02_02.js
+++ 2022/02_JAVA_Class/html/src/main/webapp/black/resources/js/day01/test02_02.js
@@ -1,3 +1,3 @@
-document.write('test02_02.js')
-document.write('<div style="color: blue; font-size:16pt">첫번째 자바스크립트 파일</div>')
-alert('Nested Script File!')
+document.write('test02_02.js 파일');
+document.write('<div style="color: green; font-size: 16pt;">두번째 파일에 추가되는 외부자바스크립트 파일</div>');
+alert('Nested Script File!');
